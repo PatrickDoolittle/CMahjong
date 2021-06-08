e6c10e49257bcbbf82a70197c0ee58ccc1987777
patrickdoolittle <patrick.doolittle@gmail.com>
2021-06-08 14:22:52 -0700

    Tileset generated

--- CMJ.c
+++ CMJ.c
@@ -1,36 +1,58 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX_TILES 136
-#define MAX_SUITS 3
-#define MAX_DRAGONS 3
-#define MAX_WINDS 4
 
 struct tile{ //Data structure for a Riichi mahjong tile
     char *suit;
-    char *value;
+    int value; 
 };
 typedef struct tile Tile; //Global namespace alias
 
 //String array of suits and honors
-char *suits[MAX_SUITS] = {"Manzu","Pinzu","Souzu"};
+char *suits[3] = {"Manzu","Pinzu","Souzu"};
 char *dragons[3] = {"Red","White","Green"};
 char *winds[4] = {"East","South","West","North"};
+
+//Function declarations
+void initialize(Tile deck[]);
 
 int main(void)
 {
     printf("Welcome to C Mahjong.\n");
-    Tile deck[MAX_TILES]; //Array of tiles for the deck
-    initialize[deck]; 
+    Tile deck[136]; //Array of tiles for the deck
+    initialize(deck); 
     return 0;
 }
 
 // Function to initialize a new set of tiles.
 void initialize(Tile deck[]){
-    int suitedTiles = MAX_SUITS * 9 * 4;
-    for(int i = 0;i<suitedTiles;i+=4){
-        deck[i].suit = suits[i%MAX_SUITS]; 
+    int deckIndex = 0;
+    //Initialize suited tiles
+    for(int i=0;i<3;i++){
+        for(int j=1;j<10;j++){
+            for(int k=0;k<4;k++){
+                deck[deckIndex].suit = suits[i];
+                deck[deckIndex].value = j;
+		printf("Tile at index %d has suit %s and value %d\n", deckIndex, deck[deckIndex].suit, deck[deckIndex].value);
+                deckIndex++;
+            }
+        }
     }
-    for(int i=suitedTiles;i<MAX_DRAGONS;i++){
-    
+    //Initialize dragons
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            deck[deckIndex].suit = dragons[i];
+            deck[deckIndex].value = 0;
+	    printf("Tile at index %d has suit %s and value %d\n", deckIndex, deck[deckIndex].suit, deck[deckIndex].value);
+            deckIndex++;
+        }
+    }
+    //Initialize winds
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            deck[deckIndex].suit = winds[i];
+            deck[deckIndex].value = 0;
+	    printf("Tile at index %d has suit %s and value %d\n", deckIndex, deck[deckIndex].suit, deck[deckIndex].value);
+            deckIndex++;
+        }
     }
 }
